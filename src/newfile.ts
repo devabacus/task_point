2a4475d533119068537ef0a5c98763f989be4aab
devabacus <devabacusnrf@gmail.com>
2024-01-18 17:33:52 +0500

    1

--- src/newfile.ts
+++ src/newfile.ts
@@ -1,7 +1,7 @@
 function calEvent(): void {
     const options = {
         description: 'Описание2',
-        location: 'Локация2'
+        location: 'Локация3'
     };
     createCalendarEvent('Новое событие3', new Date('2024-01-17T10:00:00'), new Date('2024-01-17T11:00:00'), options)
 }
